ce0f217e89bf58c07c91e171d8b056bf2463abf0
johannes60 <68226329+johannes60-sk@users.noreply.github.com>
2025-07-05 20:51:32 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 
   // get port from environment variables or use default
   const configService = app.get<AppConfigService>(AppConfigService);
-  const port = process.env.PORT || 1339;
+  const port = normalizePort(configService.port);
 
   await app.listen(port);
   logger.log(`Skill Quest API v${version} started`);
